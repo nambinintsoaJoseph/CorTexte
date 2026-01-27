a360cb16fc9684bb77a8f7a430d689ac1426981b
Nambinintsoa <raznambinintsoa3@gmail.com>
2026-01-27 23:06:51 +0300

    chore(deps): custom font and fontawesome setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,15 @@ export default {
     "./src/**/*.{js,jsx,ts,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'green-cortexte': '#009C65'
+      },
+      fontFamily: {
+        inter: ['Inter', 'sans-serif'],
+        outfit: ['Outfit', 'sans-serif'],
+      }
+    },
   },
   plugins: [],
 }
